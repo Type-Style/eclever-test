3aca405dbd0cf70cfa2f53eb4410d20bc9c96fda
Type-Style <github@type-style.de>
2025-02-06 12:22:32 +0100

    [Task] add api calls

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 // ! Terrible workaround for deprecated mapbox lib
 // ! See explanation here: https://github.com/alex3165/react-mapbox-gl/issues/822#issuecomment-1076315575
 import * as mapboxgl from "mapbox-gl";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const accessToken =
   "pk.eyJ1IjoiZWNsZXZlciIsImEiOiJja3IzM3B3b24yMHNsMnBueGNya3I4eXExIn0.qNBd6dRRZLTTxKSJ0PUazg";
 //@ts-ignore
@@ -13,9 +14,13 @@ Object.getOwnPropertyDescriptor(mapboxgl, "accessToken").set(accessToken);
 
 const container = document.getElementById("root");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+const queryClient = new QueryClient()
+
 
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
